fe57da834e912d93f03c45a0e7769da99419050a
Oregunwa Oluwadamilola Michael <moregunwa@gmail.com>
2024-05-09 16:21:43 +0100

    Finished queue

--- src/queuemodel/__init__.py
+++ src/queuemodel/__init__.py
@@ -1,48 +1,84 @@
-import math
-import rich
-from rich.console import Console
-from rich.prompt import Confirm, FloatPrompt, IntPrompt
-
-
-def model_queue(console: Console):
-    arrival_rate = FloatPrompt.ask("[blue]Enter arrival rate per hour")
-
-    while arrival_rate <= 0:
-        console.print("[red]Arrival rate must be greater than 0")
-        arrival_rate = FloatPrompt.ask("[blue]Enter arrival rate per hour")
-
-    service_rate = FloatPrompt.ask("[blue]Enter service rate: ")
-
-    while service_rate <= 0:
-        console.print("[red]Service rate must be greater than 0")
-        service_rate = FloatPrompt.ask("[blue]Enter service rate per hour")
-
-    pho = arrival_rate / service_rate
-
-    n = IntPrompt.ask("[blue]Input the number of people: ")
-
-    p = 1 - pho
-
-    pn = math.pow(p, n) * p
-
-    console.print(f"\n[green]For {n} number of people, the answer is {pn}")
-
-    len_of_system = pho / p
-    len_of_queue = len_of_system - pho
-    wait_of_system = len_of_system / arrival_rate
-    wait_of_queue = len_of_queue / arrival_rate
-
-    console.print(f"\n[green]The length of the system is {len_of_system}")
-    console.print(f"[green]The length of the queue is {len_of_queue}")
-    console.print(f"[green]The waiting time of the system is {wait_of_system}")
-    console.print(f"[green]The waiting time of the queue is {wait_of_queue}")
+import queuemodel.queue as queuelib
 
 
 def main():
-    console = rich.get_console()
     running = True
     while running:
-        console.clear()
-        model_queue(console)
-        running = Confirm.ask("\nDo you still want to continue")
-    console.print("\n[green]Thank you for running")
+        try:
+            queue_length = int(
+                input("What is the length of the queue (0 for infinite):")
+            )
+
+            arrival_rate = float(input("What is the arrival rate: "))
+            while arrival_rate < 0:
+                print("Arrival rate must be greater than 0")
+                arrival_rate = float(input("What is the arrival rate: "))
+
+            service_rate = float(input("What is the service rate: "))
+            while service_rate < 0:
+                print("Service rate must be greater than 0")
+                service_rate = float(input("What is the service rate: "))
+
+            queue = queuelib.Queue(
+                arrival_rate,
+                service_rate,
+                queue_length=queue_length if queue_length > 0 else None,
+            )
+
+            print("The length of the queue", queue.length_of_the_queue())
+            print("The length of the system", queue.length_of_the_system())
+            print("The waiting time of the queue", queue.waiting_time_of_queue())
+            print("The waiting time of the system", queue.waiting_time_of_system())
+
+            no_person_in_queue = queue.probability_that_no_queue()
+            people_in_system = int(input("\nHow many people are in the system: "))
+
+            if queue.queue_length is not None:  # If the queue is finite
+                while queue.queue_length > people_in_system:
+                    print("Too large to be in system")
+                    people_in_system = int(
+                        input("\nHow many people are in the system: ")
+                    )
+
+            prob_people_in_system = queue.probability_of_no_of_people_in_system(
+                people_in_system
+            )
+
+            at_least_people_in_system = queue.at_least_people_in_system(
+                people_in_system
+            )
+            at_most_people_in_system = queue.at_most_people_in_system(people_in_system)
+
+            print(f"The probablity of nobody in the queue is {no_person_in_queue}")
+
+            print(
+                f"The probablity of {people_in_system} in the system is {prob_people_in_system}"
+            )
+
+            print(
+                f"The probablity of at least {people_in_system} in the system is {at_least_people_in_system}"
+            )
+
+            print(
+                f"The probablity of at most {people_in_system} in the system is {at_most_people_in_system}"
+            )
+            if queue.queue_length is not None:  # If the queue is finite
+                doesnt_join = queue.probability_of_no_of_people_in_system(10)
+                join = round(1 - doesnt_join, 4)
+                print(
+                    f"The probablity of a person that comes doesn't the queue is {doesnt_join}"
+                )
+
+                print(
+                    f"The probablity of a person that comes joining the queue is {join}"
+                )
+
+            print("\nYou have reached the end of the program\n")
+
+            running = (
+                input("Press enter to start again (back to quit): ").lower() != "back"
+            )
+
+        except Exception:
+            print("Something went wrong")
+            running = False
---
+++ src/queuemodel/queue.py
@@ -0,0 +1,105 @@
+import math
+from typing import Optional
+
+
+class Queue:
+    def __init__(
+        self,
+        arrival_rate: float,
+        service_rate: float,
+        queue_length: Optional[int] = None,
+    ):
+        self.arrival_rate = arrival_rate
+        self.service_rate = service_rate
+        self.rho = round(arrival_rate / service_rate, 4)
+        self.queue_length = queue_length
+
+    def effective_arrival_rate(self) -> float:
+        answer = 0
+        if self.queue_length is not None:
+            answer = self.arrival_rate * (
+                1 - self.probability_of_no_of_people_in_system(self.queue_length)
+            )
+
+        return round(answer, 4)
+
+    def probability_of_no_of_people_in_system(self, no_of_person: int) -> float:
+        probability = 0
+        if self.queue_length is None:  # If the queue is infinite
+            probability = math.pow(self.rho, no_of_person) * (1 - self.rho)
+        else:
+            probability = math.pow(self.rho, no_of_person) * (
+                (1 - self.rho) / (1 - math.pow(self.rho, self.queue_length + 1))
+            )
+        return round(probability, 4)
+
+    def length_of_the_system(self):
+        length = 0
+        if self.queue_length is None:  # If the queue is infinite
+            length = self.rho / (1 - self.rho)
+        else:
+            numerator = self.rho * (
+                1
+                + self.queue_length * math.pow(self.rho, self.queue_length + 1)
+                - (self.queue_length + 1) * math.pow(self.rho, self.queue_length)
+            )
+            denominator = (1 - self.rho) * (
+                1 - math.pow(self.rho, self.queue_length + 1)
+            )
+            length = numerator / denominator
+        return round(length, 4)
+
+    def length_of_the_queue(self):
+        answer = 0
+        if self.queue_length is None:  # If the queue is infinite
+
+            answer = self.length_of_the_system() - self.rho
+        else:
+            answer = self.length_of_the_system() - (
+                self.effective_arrival_rate() / self.service_rate
+            )
+
+        return round(answer, 4)
+
+    def waiting_time_of_system(self):
+        answer = 0
+        if self.queue_length is None:  # If the queue is infinite
+            answer = self.length_of_the_system() / self.arrival_rate
+        else:
+            answer = self.length_of_the_system() / self.effective_arrival_rate()
+
+        return round(answer, 4)
+
+    def waiting_time_of_queue(self):
+        answer = 0
+        if self.queue_length is None:  # If the queue is infinite
+            answer = self.length_of_the_queue() / self.arrival_rate
+        else:
+            answer = self.length_of_the_queue() / self.effective_arrival_rate()
+
+        return round(answer, 4)
+
+    def probability_that_no_queue(self):
+        answer = self.probability_of_no_of_people_in_system(
+            0
+        ) + self.probability_of_no_of_people_in_system(1)
+        return round(answer, 4)
+
+    def at_least_people_in_system(self, people_in_system: int):
+        """
+        To find the probability that at least x people would be in the system
+        We need to find the probability that at most x people are in the system and then
+        negate it from 1
+        """
+        answer = 1 - self.at_most_people_in_system(people_in_system - 1)
+        return round(answer, 4)
+
+    def at_most_people_in_system(self, people_in_system: int):
+        """
+        To find the probability that at most x people would be in the system
+        We need to sum up all the probabilities up to x people
+        """
+        answer = 0
+        for i in range(people_in_system + 1):
+            answer += self.probability_of_no_of_people_in_system(i)
+        return round(answer, 4)
